52cbfdbde99bc5389a0fa85a36681f094ab18b04
pierresouza <pierre.s3@hotmail.com>
2026-08-21 13:30:11 -0300

    feat: integrate TypeORM with Postgres using custom configuration service in AppModule

---
+++ src/config/postgres.config.service.ts
@@ -0,0 +1,16 @@
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+export class PostgresConfigService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'loja',
+      entities: [],
+      synchronize: true,
+    };
+  }
+}
